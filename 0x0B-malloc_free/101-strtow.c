823b5fa60754be5c631b387ae24e3ba548ce0bfa
KhalifahdGreat <abubakaronumoh@gmail.com>
2024-07-16 14:09:21 -0700

    all

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,22 +9,22 @@
  */
 int count_words(char *str)
 {
-	int count = 0, in_word = 0;
+    int count = 0, in_word = 0;
 
-	while (*str)
-	{
-		if (*str == ' ')
-		{
-			in_word = 0;
-		}
-		else if (!in_word)
-		{
-			in_word = 1;
-			count++;
-		}
-		str++;
-	}
-	return (count);
+    while (*str)
+    {
+        if (*str == ' ')
+        {
+            in_word = 0;
+        }
+        else if (!in_word)
+        {
+            in_word = 1;
+            count++;
+        }
+        str++;
+    }
+    return (count);
 }
 
 /**
@@ -37,18 +37,18 @@ int count_words(char *str)
  */
 char *allocate_word(char *str, int start, int end)
 {
-	char *word;
-	int i;
+    char *word;
+    int i;
 
-	word = malloc((end - start + 1) * sizeof(char));
-	if (word == NULL)
-		return (NULL);
+    word = malloc((end - start + 1) * sizeof(char));
+    if (word == NULL)
+        return (NULL);
 
-	for (i = 0; start < end; start++, i++)
-		word[i] = str[start];
-	word[i] = '\0';
+    for (i = 0; start < end; start++, i++)
+        word[i] = str[start];
+    word[i] = '\0';
 
-	return (word);
+    return (word);
 }
 
 /**
@@ -60,51 +60,51 @@ char *allocate_word(char *str, int start, int end)
  */
 char **split_into_words(char *str, int word_count)
 {
-	char **words;
-	int i, j, start, in_word;
+    char **words;
+    int i, j, start, in_word, k;
 
-	words = malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
+    words = malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return (NULL);
 
-	for (i = 0, j = 0, start = 0, in_word = 0; str[i]; i++)
-	{
-		if (str[i] == ' ')
-		{
-			if (in_word)
-			{
-				words[j] = allocate_word(str, start, i);
-				if (words[j++] == NULL)
-				{
-					for (int k = 0; k < j - 1; k++)
-						free(words[k]);
-					free(words);
-					return (NULL);
-				}
-				in_word = 0;
-			}
-		}
-		else if (!in_word)
-		{
-			start = i;
-			in_word = 1;
-		}
-	}
+    for (i = 0, j = 0, start = 0, in_word = 0; str[i]; i++)
+    {
+        if (str[i] == ' ')
+        {
+            if (in_word)
+            {
+                words[j] = allocate_word(str, start, i);
+                if (words[j++] == NULL)
+                {
+                    for (k = 0; k < j - 1; k++)
+                        free(words[k]);
+                    free(words);
+                    return (NULL);
+                }
+                in_word = 0;
+            }
+        }
+        else if (!in_word)
+        {
+            start = i;
+            in_word = 1;
+        }
+    }
 
-	if (in_word)
-	{
-		words[j] = allocate_word(str, start, i);
-		if (words[j++] == NULL)
-		{
-			for (int k = 0; k < j - 1; k++)
-				free(words[k]);
-			free(words);
-			return (NULL);
-		}
-	}
-	words[j] = NULL;
+    if (in_word)
+    {
+        words[j] = allocate_word(str, start, i);
+        if (words[j++] == NULL)
+        {
+            for (k = 0; k < j - 1; k++)
+                free(words[k]);
+            free(words);
+            return (NULL);
+        }
+    }
+    words[j] = NULL;
 
-	return (words);
+    return (words);
 }
 
 /**
@@ -115,15 +115,15 @@ char **split_into_words(char *str, int word_count)
  */
 char **strtow(char *str)
 {
-	int word_count;
+    int word_count;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
+    if (str == NULL || *str == '\0')
+        return (NULL);
 
-	word_count = count_words(str);
-	if (word_count == 0)
-		return (NULL);
+    word_count = count_words(str);
+    if (word_count == 0)
+        return (NULL);
 
-	return (split_into_words(str, word_count));
+    return (split_into_words(str, word_count));
 }
 
