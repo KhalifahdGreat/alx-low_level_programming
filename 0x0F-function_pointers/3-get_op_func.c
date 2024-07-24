31b57ad634d49b5b51074e5446135fa17265a1d7
KhalifahdGreat <abubakaronumoh@gmail.com>
2024-07-24 15:16:29 -0700

    all

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * get_op_func - matches operator from main
+ * @s: op str
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t op_s[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (op_s[i].op != NULL && *(op_s[i].op) != *s)
+i++;
+return (op_s[i].f);
+}
